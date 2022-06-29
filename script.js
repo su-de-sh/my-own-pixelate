e8f7ace0ea683fd30b746c33ba4bb5f04664047d
su-de-sh <sudesh7443@gmail.com>
2022-06-29 21:40:38 +0545

    added all features

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Your code here
-
+let isMouseDown = false;
+let columnNum;
 function makeRow() {
-  //   console.log("im clicked");
-  let columnNum = document.querySelector("#input").value;
-  //   console.log(columnNum);
+  //   let columnNum = document.querySelector("#input").value;
+  if (!columnNum) {
+    columnNum = prompt("Enter the number of column in canvas!!");
+  }
   const row = document.createElement("tr");
   for (let i = 0; i < columnNum; i++) {
     const column = document.createElement("td");
@@ -16,16 +18,30 @@ function makeRow() {
 }
 
 function colorize(e) {
+  if (e.target.id === "clear-color") {
+    Array.from(document.querySelectorAll("td")).forEach((element) => {
+      element.className = "";
+    });
+  }
   color = document.querySelector("select").value;
-
-  if (e.target.nodeName === "TD") {
-    console.log(e.target.classList);
-    if (e.target.className === color) {
-      e.target.className = "";
-    } else e.target.className = color;
+  if (isMouseDown) {
+    if (e.target.nodeName === "TD") {
+      if (e.target.className === color) {
+        e.target.className = "";
+      } else e.target.className = color;
+    }
   }
 }
 
 document.querySelector("#add-row").addEventListener("click", makeRow);
 
-document.querySelector("#table").addEventListener("click", colorize);
+document.querySelector("#table").addEventListener("mouseover", colorize);
+document.querySelector("body").addEventListener("mousedown", function (e) {
+  isMouseDown = true;
+  colorize(e);
+});
+document.querySelector("body").addEventListener("mouseup", function () {
+  isMouseDown = false;
+});
+
+document.querySelector("#clear-color").addEventListener("click", colorize);
