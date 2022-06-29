8b771644441b4f4fd2de8f346ab1f86a792fd989
su-de-sh <sudesh7443@gmail.com>
2022-06-29 11:57:29 +0545

    added addrow feature

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your code here
+
+function makeRow() {
+  //   console.log("im clicked");
+  let columnNum = document.querySelector("#input").value;
+  //   console.log(columnNum);
+  const row = document.createElement("tr");
+  for (let i = 0; i < columnNum; i++) {
+    const column = document.createElement("td");
+    row.appendChild(column);
+  }
+
+  const table = document.querySelector("table");
+
+  table.appendChild(row);
+}
+
+document.querySelector("#add-row").addEventListener("click", function () {
+  makeRow();
+});
